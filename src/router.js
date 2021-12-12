4e0c45a53185005fcc73cf8a5c4c12149a154b16
John Steven Bernal Gonzalez <bernalgonzalezjohn@gmail.com>
2021-12-11 19:26:39 -0500

    reparando rending

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
 import Instrucciones from "./components/Instrucciones.vue";
-import SeleccionarUbicacion from "./components/SeleccionarUbicacion.vue";
+// import SeleccionarUbicacion from "./components/SeleccionarUbicacion.vue";
 import CrearAfiliado from "./components/CrearAfiliado.vue";
-import CrearSeguimiento from "./components/CrearSeguimiento.vue";
-import FiltrarEstado from "./components/FiltrarEstado.vue";
-import ModificarSeguimiento from "./components/ModificarSeguimiento.vue";
+// import CrearSeguimiento from "./components/CrearSeguimiento.vue";
+// import FiltrarEstado from "./components/FiltrarEstado.vue";
+// import ModificarSeguimiento from "./components/ModificarSeguimiento.vue";
 import IniciarSesion from "./components/IniciarSesion.vue";
 import CerrarSesion from "./components/CerrarSesion.vue";
 import CrearNuevoUsuario from "./components/CrearNuevoUsuario.vue";
@@ -63,37 +63,36 @@ const routes = [
     name: "EliminarCuenta",
     component: EliminarCuenta,
   },
-  {
-    path: "/crear/ubicacion",
-    name: "SeleccionarUbicacion",
-    component: SeleccionarUbicacion,
-  },
-  {
-    path: "/crear/registro",
-    name: "CrearRegistro",
-    component: CrearRegistro,
-  },
-  {
-    path: "/crear/seguimiento",
-    name: "CrearSeguimiento",
-    component: CrearSeguimiento,
-  },
-  {
-    path: "/filtrar/sexo",
-    name: "FiltrarSexo",
-    component: FiltrarSexo,
-  },
-  {
-    path: "/filtrar/estado",
-    name: "FiltrarEstado",
-    component: FiltrarEstado,
-  },
-
-  {
-    path: "/ModificarSeguimiento",
-    name: "ModificarSeguimiento",
-    component: ModificarSeguimiento,
-  },
+  // {
+  //   path: "/crear/ubicacion",
+  //   name: "SeleccionarUbicacion",
+  //   component: SeleccionarUbicacion,
+  // },
+  // {
+  //   path: "/crear/registro",
+  //   name: "CrearRegistro",
+  //   component: CrearRegistro,
+  // },
+  // {
+  //   path: "/crear/seguimiento",
+  //   name: "CrearSeguimiento",
+  //   component: CrearSeguimiento,
+  // },
+  // {
+  //   path: "/filtrar/sexo",
+  //   name: "FiltrarSexo",
+  //   component: FiltrarSexo,
+  // },
+  // {
+  //   path: "/filtrar/estado",
+  //   name: "FiltrarEstado",
+  //   component: FiltrarEstado,
+  // },
+  // {
+  //   path: "/ModificarSeguimiento",
+  //   name: "ModificarSeguimiento",
+  //   component: ModificarSeguimiento,
+  // },
 ];
 
 const router = createRouter({
